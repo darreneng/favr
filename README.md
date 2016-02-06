29633253f3928b0d7fb0c290bddd621c2140d79f
Darren Eng <darrenle9@gmail.com>
2016-02-05 18:13:39 -0800

    added update functionality

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-lab4
-====
+FAVR - A social hub for favors between friends.
 
-Lab 4: Programming with Javascript
+App created for CSE 170: Introduction to Human Computer Interaction
+
+Created by Manav, Filbert, and Darren
+
+Deployed here:
+http://favrfam.herokuapp.com/
--- routes/offer.js
+++ routes/offer.js
@@ -1,3 +1,8 @@
+var data = require("../data.json");
 exports.view = function(req, res) {
   res.render('offer');
+  var newOffer = {"name": req.query.name, "task": req.query.task,
+                  "date": req.query.duedate};
+  console.log("request received");
+  data['favrs'].push(newOffer);
 };
