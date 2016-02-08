e3503caf974136ca5fac5ed1af7fdddf5f123a61
vagrant <vagrant@precise32.(none)>
2016-02-08 08:05:25 +0000

    Added delete skeleton

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
@@ -9,5 +10,19 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	console.log("REACHED");
+	$('.delete').click(deleteFavr);
 	// add any functionality and listeners you want here
+}
+
+function deleteFavr(e){
+	e.preventDefault();
+	var yes = confirm("Are you sure you want to delete this Favr?");
+	if(yes){
+		console.log("yes");
+	}
+	else{
+		console.log("nah");
+	}
+
 }
