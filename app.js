4d32a3836d9154722e39106d96dabf4b74ab2b2c
vagrant <vagrant@precise32.(none)>
2016-02-11 21:18:02 +0000

    reload

--- app.js
+++ app.js
@@ -54,3 +54,4 @@ app.get('/completedFavrs', completedFavrs.addCompletedFavr);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+//
