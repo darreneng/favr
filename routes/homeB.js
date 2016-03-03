f12c43c403eddab3939cc0538d8ef6dddfadabb3
Filbert <fjahja@ucsd.edu>
2016-03-03 21:18:55 +0000

    Readded homeB

---
+++ routes/homeB.js
@@ -0,0 +1,19 @@
+var data = require('../data.json');
+var userdata = require('../public/js/userdata');
+
+
+exports.view = function(req, res){
+	//Find the user_id currently logged on
+	var user_id = -1;
+	for(var i = 0; i < data['users'].length; i++){
+		if(data['users'][i]['active']){
+			user_id = i;
+		}
+	}
+
+
+
+	var user = userdata.getUserData(data, user_id);
+
+	res.render('homeB', user);
+};
