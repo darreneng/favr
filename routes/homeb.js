dd5810a0cf19a8a5f941c7d3b4498f3d0228604b
Filbert <fjahja@ucsd.edu>
2016-03-03 21:06:36 +0000

    Changed homeb to homeB

--- routes/homeb.js
+++ routes/homeb.js
@@ -1,9 +1,6 @@
-/*
- * GET home page.
- */
-
 var data = require('../data.json');
 var userdata = require('../public/js/userdata');
+
 
 exports.view = function(req, res){
 	//Find the user_id currently logged on
@@ -13,8 +10,8 @@ exports.view = function(req, res){
 			user_id = i;
 		}
 	}
-	//var user_id = 1;
+
 	var user = userdata.getUserData(data, user_id);
 
-	res.render('homeb', user);
+	res.render('homeB', user);
 };
